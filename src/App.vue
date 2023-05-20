145e62411ad53e3dac9c5ddf01000f205020de83
Aleks <aleksandar.trpkovski@gmail.com>
2023-05-20 20:55:36 +1000

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import vFileDrop from "./components/v-file-drop.vue";
+</script>
+
+<template>
+    <div>
+        <v-file-drop multiple>
+            <div class="file-upload"></div>
+        </v-file-drop>
+    </div>
+</template>
+
+<style scoped>
+.file-upload {
+    width: 500px;
+    height: 500px;
+    background-color: blue;
+}
+</style>
