57fa37e26b72aa34942813bfd44758336e2ed722
Aleks <aleksandar.trpkovski@gmail.com>
2023-05-21 18:14:13 +1000

    clean up vite plugins that are not needed

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,11 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import typescript from "rollup-plugin-typescript2";
-// import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         vue(),
-        // dts({
-        //     insertTypesEntry: true,
-        // }),
         typescript({
             check: false,
             include: ["src/components/*.vue"],
